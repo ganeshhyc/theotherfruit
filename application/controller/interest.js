6dbe313742cc1512666b819fc528e5b8a67ed120
ganeshhyc <id.ganesh123@gmail.com>
2019-05-21 17:43:38 +0530

    boom

--- application/controller/interest.js
+++ application/controller/interest.js
@@ -1,3 +1,7 @@
 app.controller('interestCtrl',function($scope){
     $scope.interest_arr = ["tattoo.png","art.png","model.png","body_artist.png","performance.png","venue_pic.png","supplier_pic.png","business_pic.png"];
+    $scope.finished = false;
+    $scope.finish = function(){
+        $scope.finished = true;
+    }
 })
